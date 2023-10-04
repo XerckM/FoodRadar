c11f660c2e68373d7a41570e369568bb1d826ac0
Xerckiem <mercadoxerckiem@gmail.com>
2023-10-04 11:36:44 -0400

    Added cookies

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
 const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 const dotenv = require('dotenv').config({path: "./vars/.env"});
+
 const PORT = process.env.PORT || 4000;
 const userRouter = require("./routes/userRoutes");
-const bodyParser = require('body-parser');
-const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
-app.use(cors());
 dbConnect();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 
