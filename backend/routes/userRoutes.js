b24974a2b134a62d0eab6a1f2b970b8dc7030abc
Xerckiem <mercadoxerckiem@gmail.com>
2023-10-04 11:34:01 -0400

    Added new routes for handling refresh token and logout

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,18 +7,27 @@ const {
     deleteUser, 
     updateUser, 
     blockUser,
-    unblockUser
+    unblockUser,
+    handleRefreshToken,
+    logout,
+    updatePassword
 } = require("../controller/userController");
-const { authMiddleware, isAdmin, restrictTo } = require("../middlewares/authMiddleware");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+
 router.get('/all-users', authMiddleware, isAdmin, getAllUsers)
+router.get('/refresh', handleRefreshToken);
+router.get("/logout", logout);
 router.get('/:id', authMiddleware, isAdmin, getUser);
+
 router.delete('/:id', deleteUser);
+
 router.put('/edit-user', authMiddleware, updateUser);
-router.put('/block-user/:id', authMiddleware, restrictTo("admin"), blockUser);
-router.put('/unblock-user/:id', authMiddleware, restrictTo("admin"), unblockUser);
+router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
+router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
+router.put('/password', authMiddleware, updatePassword)
 
 module.exports = router;
