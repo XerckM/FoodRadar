a6c8ae6aa7d5aae980f6f50e13310fb27274ae45
chris <cy.chrisyue@gmail.com>
2023-11-03 18:44:21 -0400

    added divs to Navbar.js for future tasks

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,23 +4,27 @@ import { useState } from "react";
 export const Navbar = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/mainpage">MAP</Link>
-        </li>
-        <li>
-          <Link to="/profile">PROFILE</Link>
-        </li>
-        <li>
-          <Link to="/login">LOGIN</Link>
-        </li>
-        <li>
-          <Link to="/logout">LOGOUT</Link>
-        </li>
-      </ul>
+      <Link to="/">
+        <p1>Home</p1>
+      </Link>
+      <Link to="/mainpage">
+        <p1>MainPage</p1>
+      </Link>
+      <Link to="/profile">
+        <p1>Profile</p1>
+      </Link>
+      <nav>
+        <div>
+          <Link to="/login">
+            <p1>Login</p1>
+          </Link>
+        </div>
+        <div>
+          <Link to="/logout">
+            <p1>Logout</p1>
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
