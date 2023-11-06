ee176647f0e5f38ac6514f08016696a904933ea9
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 10:45:16 -0500

    modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
-import React, { useState } from 'react';
-import { UserActions } from './components/UserActions';
+import React from 'react';
+import { AppRoutes } from './routes/Router';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLoginSuccess = () => {
-    setTimeout(() => {
-      setIsLoggedIn(true);
-    }, 2000); // Display the proxy page for 2 seconds
-  };
-
   return (
-    <div className="App">
-      {isLoggedIn ? (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#f7f7f7', fontSize: '24px', color: '#007BFF' }}>
-          Welcome to Food Radar!
-        </div>
-      ) : (
-        <UserActions onLoginSuccess={handleLoginSuccess} />
-      )}
-    </div>
-  );
+    <AppRoutes />
+  )
 }
 
 export default App;
