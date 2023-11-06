a655260f20c25ec5646af0e5986a23eebd736e54
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 11:43:49 -0500

    modified router

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,15 +1,18 @@
+// Router.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomeView } from '../views/Home/HomeView';
 import { SignUpView } from '../views/Signup/SignUpView';
 import { LoginView } from '../views/Login/LoginView';
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/signup" element={<SignUpView />} /> // Use SignUpView here
-        <Route path="/login" element={<LoginView />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/signup" element={<SignUpView />} />
+          <Route path="/login" element={<LoginView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
