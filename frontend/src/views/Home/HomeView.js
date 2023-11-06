9f57689357642cf5d99b8c892c9f761aae92308b
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 14:56:42 -0500

    modified home view js and css

--- frontend/src/views/Home/HomeView.js
+++ frontend/src/views/Home/HomeView.js
@@ -1,11 +1,10 @@
 import React from "react";
-// import { HomeActions } from "../../components/HomeActions";
 import "./HomeView.css";
+import background from "../../images/background.jpg";
 
 export const HomeView = () => {
     return (
         <div className="full-page">
-            <h2 className="title">Welcome to Food Radar</h2>
             <header className="navbar">
                 <div className="nav-buttons">
                     <a href="/signup" className="signup-button">Sign Up</a>
@@ -14,7 +13,7 @@ export const HomeView = () => {
             </header>
             <div className="main-content">
                 <div className="image-container">
-                    <img src="https://media.istockphoto.com/id/475511846/vector/kitchen-seamless-pattern-vector-background.jpg?s=612x612&w=0&k=20&c=inpW5Mc2MFyuc7PsMXVY49OUBU39EXekcCNe8xVeI_k=" alt="Background" />
+                    <img className="background-image" src={ background } alt="Background" />
                     <div className="search-bar">
                         <input type="text" placeholder="Search..." />
                     </div>
