d401903bede3ac059398afc4b1edaa26b78b7b76
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 20:53:04 -0500

    modified navbar

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+export const Navbar = ({ showAuthButtons }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return (
+        <>
+            <header className={`navbar ${isMenuOpen ? 'hide' : ''}`}>
+                <div className="nav-left">
+                    <button className="hamburger-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>☰</button>
+                    <span className="nav-title">Food Radar</span>
+                </div>
+                {showAuthButtons && <div className="nav-buttons">
+                    <a href="/signup" className="signup-button">Sign Up</a>
+                    <a href="/login" className="login-button">Login</a>
+                </div>}
+            </header>
+            {isMenuOpen && <div className="overlay" onClick={() => setIsMenuOpen(false)}></div>}
+            <aside className={isMenuOpen ? "menu open" : "menu"}>
+                <a href="/signup" className="menu-button signup">Sign Up</a>
+                <a href="/login" className="menu-button login">Login</a>
+            </aside>
+        </>
+    );
+};
