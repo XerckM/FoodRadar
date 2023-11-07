0d3ac9623b3a49c41cf7cce6474e7f644d862927
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 20:54:39 -0500

    separated navbar code and imported as an object

--- frontend/src/views/Home/HomeView.js
+++ frontend/src/views/Home/HomeView.js
@@ -1,18 +1,11 @@
 import React from "react";
 import "./HomeView.css";
+import { Navbar } from "../../components/Navbar/Navbar";
 
 export const HomeView = () => {
     return (
         <div className="full-page-home">
-            <header className="navbar-home">
-                <div className="nav-left-home">
-                    <button className="hamburger-button-home">☰</button>
-                    <span className="nav-title-home">Food Radar</span>
-                </div>
-                <div className="nav-buttons-home">
-                    {/* Add a link to the ProfileView here */}
-                </div>
-            </header>
+            <Navbar showAuthButtons={false}/>
             <div className="main-content-home">
                 <div className="search-bar">
                     <input type="text" placeholder="Search..." />
--- frontend/src/views/Index/IndexView.js
+++ frontend/src/views/Index/IndexView.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./IndexView.css";
+import { Navbar } from "../../components/Navbar/Navbar";
 
 export const IndexView = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
     return (
         <div className="full-page">
-            <header className={`navbar ${isMenuOpen ? 'hide' : ''}`}>
-                <div className="nav-left">
-                    <button className="hamburger-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>☰</button>
-                    <span className="nav-title">Food Radar</span>
-                </div>
-                <div className="nav-buttons">
-                    <a href="/signup" className="signup-button">Sign Up</a>
-                    <a href="/login" className="login-button">Login</a>
-                </div>
-            </header>
-            {isMenuOpen && <div className="overlay" onClick={() => setIsMenuOpen(false)}></div>}
-            <aside className={isMenuOpen ? "menu open" : "menu"}>
-                <a href="/signup" className="menu-button signup">Sign Up</a>
-                <a href="/login" className="menu-button login">Login</a>
-            </aside>
+            <Navbar showAuthButtons={true}/>
             <div className="main-content">
                 <h1>Food Radar</h1>
                 <h2>Find the best food in your area!</h2>
