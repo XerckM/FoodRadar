ef6b757fb62ffc14b372b1ece288b154d34cf31d
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 19:12:29 -0500

    modified home view

--- frontend/src/views/Home/HomeView.js
+++ frontend/src/views/Home/HomeView.js
@@ -1,22 +1,21 @@
 import React from "react";
 import "./HomeView.css";
-import background from "../../images/background.jpg";
 
 export const HomeView = () => {
     return (
         <div className="full-page">
             <header className="navbar">
+                <div className="nav-left">
+                    <button className="hamburger-button">☰</button>
+                    <span className="nav-title">Food Radar</span>
+                </div>
                 <div className="nav-buttons">
-                    <a href="/signup" className="signup-button">Sign Up</a>
-                    <a href="/login" className="login-button">Login</a>
+                    {/* Add a link to the ProfileView here */}
                 </div>
             </header>
             <div className="main-content">
-                <div className="image-container">
-                    <img className="background-image" src={ background } alt="Background" />
-                    <div className="search-bar">
-                        <input type="text" placeholder="Search..." />
-                    </div>
+                <div className="search-bar">
+                    <input type="text" placeholder="Search..." />
                 </div>
             </div>
         </div>
