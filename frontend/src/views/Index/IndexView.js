db89ce49fec7ffa9d13ab2685d044ba7955b1a28
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 20:07:22 -0500

    modified index view js and css

--- frontend/src/views/Index/IndexView.js
+++ frontend/src/views/Index/IndexView.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./IndexView.css";
 
 export const IndexView = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <div className="full-page">
-            <header className="navbar">
+            <header className={`navbar ${isMenuOpen ? 'hide' : ''}`}>
                 <div className="nav-left">
-                    <button className="hamburger-button">☰</button>
+                    <button className="hamburger-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>☰</button>
                     <span className="nav-title">Food Radar</span>
                 </div>
                 <div className="nav-buttons">
@@ -14,6 +16,11 @@ export const IndexView = () => {
                     <a href="/login" className="login-button">Login</a>
                 </div>
             </header>
+            {isMenuOpen && <div className="overlay" onClick={() => setIsMenuOpen(false)}></div>}
+            <aside className={isMenuOpen ? "menu open" : "menu"}>
+                <a href="/signup" className="menu-button signup">Sign Up</a>
+                <a href="/login" className="menu-button login">Login</a>
+            </aside>
             <div className="main-content">
                 <h1>Food Radar</h1>
                 <h2>Find the best food in your area!</h2>
