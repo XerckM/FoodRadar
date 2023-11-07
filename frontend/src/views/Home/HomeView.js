8ff199a39a6dcd84c6387531af50b4740c60ec40
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 21:19:03 -0500

    extracted and imported search as an object

--- frontend/src/views/Home/HomeView.js
+++ frontend/src/views/Home/HomeView.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./HomeView.css";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { Search } from "../../components/Search/Search";
 
 export const HomeView = () => {
     return (
         <div className="full-page-home">
             <Navbar showAuthButtons={false}/>
             <div className="main-content-home">
-                <div className="search-bar">
-                    <input type="text" placeholder="Search..." />
-                </div>
+                <Search />
             </div>
         </div>
     );
