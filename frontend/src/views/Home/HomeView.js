612f8bed08e8c3b206c5ae860e37c911792c436c
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 20:06:46 -0500

    modified home view js and css

--- frontend/src/views/Home/HomeView.js
+++ frontend/src/views/Home/HomeView.js
@@ -3,17 +3,17 @@ import "./HomeView.css";
 
 export const HomeView = () => {
     return (
-        <div className="full-page">
-            <header className="navbar">
-                <div className="nav-left">
-                    <button className="hamburger-button">☰</button>
-                    <span className="nav-title">Food Radar</span>
+        <div className="full-page-home">
+            <header className="navbar-home">
+                <div className="nav-left-home">
+                    <button className="hamburger-button-home">☰</button>
+                    <span className="nav-title-home">Food Radar</span>
                 </div>
-                <div className="nav-buttons">
+                <div className="nav-buttons-home">
                     {/* Add a link to the ProfileView here */}
                 </div>
             </header>
-            <div className="main-content">
+            <div className="main-content-home">
                 <div className="search-bar">
                     <input type="text" placeholder="Search..." />
                 </div>
