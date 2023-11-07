8f9a2b2f6d2af1d44f35dfa9eb3cc59f8e1cab3a
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-06 19:11:47 -0500

    added index routing

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,17 +1,19 @@
 // Router.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomeView } from '../views/Home/HomeView';
+import { IndexView } from '../views/Index/IndexView';
 import { SignUpView } from '../views/Signup/SignUpView';
 import { LoginView } from '../views/Login/LoginView';
+import { HomeView } from '../views/Home/HomeView';
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<HomeView />} />
+          <Route path="/" element={<IndexView />} />
           <Route path="/signup" element={<SignUpView />} />
           <Route path="/login" element={<LoginView />} />
+          <Route path="/home" element={<HomeView />} />
       </Routes>
     </BrowserRouter>
   );
