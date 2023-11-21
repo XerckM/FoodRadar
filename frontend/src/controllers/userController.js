aba14605c88af50960091f2558997a70e2a52141
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-21 02:14:11 -0500

    cleaned code

--- frontend/src/controllers/userController.js
+++ frontend/src/controllers/userController.js
@@ -4,13 +4,20 @@ const apiURL = "http://localhost:8000";
 
 const UserController = {
     login: async (email, password) => {
-        const response = await axios.post(`${apiURL}/api/user/login`, { email, password }, { withCredentials: true });
-        console.log(response.data);
+        const response = await axios.post(
+            `${apiURL}/api/user/login`,
+            { email, password },
+            { withCredentials: true }
+        );
+        console.log('User logged in');
         return response;
     },
     logout: async () => {
         try {
-            await axios.get(`${apiURL}/api/user/logout`, { withCredentials: true });
+            await axios.get(
+                `${apiURL}/api/user/logout`,
+                { withCredentials: true }
+            );
             console.log('User logged out');
         } catch (error) {
             console.error('Logout error:', error);
@@ -19,10 +26,10 @@ const UserController = {
     signup: async (userData) => {
         try {
             const response = await axios.post(`${apiURL}/api/user/register`, userData);
-            return { success: true, data: response.data };
+            return { success: true, data: response };
         } catch (error) {
-            console.error('Signup failed:', error.response.data);
-            return { success: false, message: error.response.data };
+            console.error('Signup failed:', error.response);
+            return { success: false, message: error.response };
         }
     }
     // add more methods here if needed
